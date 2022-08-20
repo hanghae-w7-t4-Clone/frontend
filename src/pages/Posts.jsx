0bcb90821ebe6b95d2f21c47c07aed5f6f0c875d
hahbr88 <90291796+hahbr88@users.noreply.github.com>
2022-08-20 14:19:24 +0900

    .gitignore

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Header from '../components/Header'
+import Layout from '../components/Layout'
 
 const Posts = () => {
   return (
     <div>
-      <Header></Header>
-
+      <Header/>
     </div>
   )
 }
