ebd8ea442834285429762e178a5cb8c8706d64cd
Jungi Park <byjungy93@gmail.com>
2022-08-20 14:20:46 +0900

    update signup & footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,73 @@ import styled from "styled-components";
 
 const Footer = () => {
   return (
-    <FooterWrap>
-      <div>
-        <div>Meta</div>
-        <div></div>
-      </div>
-    </FooterWrap>
+    <FooterCon>
+      <FooterUi>
+        <FooterLi>
+          <FooterA href="">ABOUT</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">HELP</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">PRESS</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">API</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">JOBS</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">PRIVACY</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">TEMS</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">LOCATIONS</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">TOP ACCOUNTS</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">HASHTAGS</FooterA>
+        </FooterLi>
+        <FooterLi>
+          <FooterA href="">LANGUAGE</FooterA>
+        </FooterLi>
+      </FooterUi>
+      <FooterP>© 2020 PICTUREGRAM</FooterP>
+    </FooterCon>
   );
 };
 
 export default Footer;
 
-const FooterWrap = styled.div`
-  width: 100%;
-  border: 1px solid red;
-  position: fixed;
+const FooterCon = styled.div`
+  position: absolute;
+  right: 0;
   bottom: 0;
-
-`
+  left: 0;
+  padding: 1rem;
+  background-color: #efefef;
+  text-align: center;
+`;
+const FooterUi = styled.ul`
+  text-align: center;
+`;
+const FooterLi = styled.li`
+  display: inline;
+  margin-right: 5px;
+`;
+const FooterA = styled.a`
+  text-decoration: none;
+  font-size: 12px;
+  color: #00376b;
+`;
+const FooterP = styled.p`
+  margin: 10px 0;
+  text-align: center;
+  color: #8e9096;
+  font-size: 14px;
+`;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { createMember, validateId } from "../redux/modules/memberSlice";
-import axios from "axios";
+import { createMember } from "../redux/modules/memberSlice";
+
+import { FaFacebookSquare } from "react-icons/fa";
+import Footer from "../components/Footer";
 
 const SignUp = () => {
   const dispatch = useDispatch();
@@ -18,18 +20,16 @@ const SignUp = () => {
   });
 
   const onSignUpHandler = (event) => {
-    // console.log(event.target.value);
-    // console.log(event.target.name);
-    console.log(event.target)
+  
+    console.log(event.target);
     const { name, value } = event.target;
     setMember({ ...member, [name]: value });
   };
 
   const submitUserInfo = (e) => {
-    
     e.preventDefault();
 
-    console.log("Hello This is Jungi Park")
+    console.log("Hello This is Jungi Park");
     dispatch(
       createMember({
         loginId: member.loginId,
@@ -41,255 +41,202 @@ const SignUp = () => {
   };
 
   return (
-    <MainCon>
-      <PageCon>
-        <HeaderCon>
-          <HeaderLogo>Instagram</HeaderLogo>
-          <HeaderP>Sign up to see photos and videos from your friends.</HeaderP>
-          <HeaderBtn>
-            <i className="fab fa-facebook-square"></i> Log in with Facebook
-          </HeaderBtn>
-          <MiddleCon>
-            {/* <hr>
-          <p>OR</p>
-          <hr> */}
-          </MiddleCon>
-        </HeaderCon>
-        <BottomCon>
-          <form onSubmit={submitUserInfo}>
-            <BottomInput
-              type="text"
-              name="loginId"
-              value={member.loginId}
-              placeholder="Mobile Number or Email"
-              onChange={onSignUpHandler}
-            />
-            <BottomInput
-              type="text"
-              name="name"
-              value={member.name}
-              placeholder="Full Name"
-              onChange={onSignUpHandler}
-            />
-            <BottomInput
-              type="text"
-              name="nickname"
-              value={member.nickname}
-              placeholder="Username"
-              onChange={onSignUpHandler}
-            />
-            <BottomInput
-              type="password"
-              name="password"
-              value={member.password}
-              placeholder="Password"
-              onChange={onSignUpHandler}
-            />
-            <BottonBtn type="submit">회원가입</BottonBtn>
-          </form>
+    <>
+      <div style={{ backgroundColor: "#ffffff" }}>
+        <Container>
+          <SignupDiv>
+            <div>
+              <img src="https://fontmeme.com/images/instagram-new-logo.png"></img>
+              <p>친구들의 사진과 동영상을 보려면 가입하세요.</p>
+              <button>
+                <FaFacebookSquare style={{ width: "14px", height: "14px" }} />
+                Facebook으로 로그인
+              </button>
+            </div>
+            <div>
+              <span>또는</span>
+              <form onSubmit={submitUserInfo}>
+                <input
+                  type="text"
+                  name="loginId"
+                  value={member.loginId}
+                  placeholder="Mobile Number or Email"
+                  onChange={onSignUpHandler}
+                />
+                <input
+                  type="text"
+                  name="name"
+                  value={member.name}
+                  placeholder="Full Name"
+                  onChange={onSignUpHandler}
+                />
+                <input
+                  type="text"
+                  name="nickname"
+                  value={member.nickname}
+                  placeholder="Username"
+                  onChange={onSignUpHandler}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  value={member.password}
+                  placeholder="Password"
+                  onChange={onSignUpHandler}
+                />
+                <button type="submit">회원가입</button>
+              </form>
 
-          <ul>
-            <BottonLi>By signing up, you agree to our</BottonLi>
-            <BottonLi>
-              <BottonA href="">Terms</BottonA>
-            </BottonLi>
-            <BottonLi>
-              <BottonA href="">Data Policy</BottonA>
-            </BottonLi>
-            <BottonLi>and</BottonLi>
-            <BottonLi>
-              <BottonA href="">Cookies Policy</BottonA> .
-            </BottonLi>
-          </ul>
-        </BottomCon>
-        </PageCon>
-      <div className="option">
-        <p>
-          Have an account? <BottomA1 href="">Log in</BottomA1>
-        </p>
+              {/* <input placeholder="이메일 주소" ></input>
+             <input placeholder="성명" ></input>
+             <input placeholder="닉네임(4~10자)" ></input>
+             <input type="password" placeholder="비밀번호(4~16자)"></input>
+             <button>가입</button> */}
+            </div>
+          </SignupDiv>
+          <Tologindiv>
+            <span>계정이 있으신가요?</span>
+            <span onClick={() => {}}>로그인</span>
+          </Tologindiv>
+          <TodownloadDiv>
+            <div>
+              <span>앱을 다운로드 받으세요</span>
+            </div>
+            <div>
+              <img src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png"></img>
+              <img src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png"></img>
+            </div>
+          </TodownloadDiv>
+        </Container>
       </div>
-      <BottomOtherApp>
-        <BottomOtherAppP>Get the app.</BottomOtherAppP>
-        <BottomOtherBtn type="button">
-          <i className="fab fa-apple"></i> App Store
-        </BottomOtherBtn>
-        <BottomOtherBtn type="button">
-          <i className="fab fa-google-play"></i> Google Play
-        </BottomOtherBtn>
-      </BottomOtherApp>
-      <Footer>
-        <FooterUi>
-          <FooterLi>
-            <FooterA href="">ABOUT</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">HELP</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">PRESS</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">API</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">JOBS</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">PRIVACY</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">TEMS</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">LOCATIONS</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">TOP ACCOUNTS</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">HASHTAGS</FooterA>
-          </FooterLi>
-          <FooterLi>
-            <FooterA href="">LANGUAGE</FooterA>
-          </FooterLi>
-        </FooterUi>
-        <FooterP>© 2020 PICTUREGRAM</FooterP>
-      </Footer>
-    </MainCon>
+      <Footer></Footer>
+    </>
   );
 };
 
 export default SignUp;
 
-const MainCon = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-
-  background-color: #fafafa;
-`
-
-const PageCon = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-bottom: 30px;
-
-  /* margin: 40px 40px 10px 40px; */
-
+const SignupDiv = styled.div`
   border: 1px solid #dbdbdb;
-  width: 350px;
+  div {
+    &:first-child {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      img {
+        width: 55%;
+      }
+      p {
+        color: #919191;
+        font-size: 15px;
+        font-weight: 600;
+      }
+      button {
+        background-color: #2e95f6;
+        border: none;
+        border: 1px solid #dbdbdb;
+        color: white;
+        height: 30px;
+        border-radius: 4px;
+        width: 82%;
+        font-weight: 700;
+      }
+    }
+    &:last-child {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      border-top: 1px solid #dbdbdb;
+      margin-top: 20px;
+      padding: 10px 0px;
+      span {
+        position: relative;
+        bottom: 18px;
+        background-color: white;
+        padding: 2px 10px;
+      }
+      form {
+        display: table-cell;
+        text-align: center;
+        vertical-align: middle;
+      }
+      input {
+        width: 80%;
+        height: 30px;
+        background-color: #fafafa;
+        border: 1px solid #dbdbdb;
+        margin-bottom: 10px;
+        border-radius: 4px;
+        &:focus {
+          outline: none;
+        }
+      }
+      button {
+        border: 1px solid #dbdbdb;
+        color: white;
+        height: 30px;
+        border-radius: 4px;
+        background-color: #b2dffc;
+        width: 82%;
+        font-weight: 700;
+        margin-bottom: 25px;
+        &:hover {
+          background-color: #2e95f6;
+        }
+      }
+      img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        margin-top: -10px;
+        margin-bottom: 5px;
+      }
+      p {
+        margin-top: -8px;
+      }
+    }
+  }
+`;
+
+const TodownloadDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 130px;
+  }
+  div {
+    &:first-child {
+      margin: 15px 0px;
+    }
+    &:last-child {
+      width: 80%;
+      display: flex;
+      justify-content: space-around;
+    }
+  }
+`;
+
+const Tologindiv = styled.div`
+  border: 1px solid #dbdbdb;
+  margin: 10px 0px;
+  padding: 17px 10px;
+  display: flex;
+  justify-content: space-around;
+  span {
+    font-size: 13px;
+    &:last-child {
+      margin-left: -90px;
+      color: #2e97f6;
+      cursor: pointer;
+      font-weight: 600;
+    }
+  }
+`;
+
+const Container = styled.div`
+  width: 23%;
+  margin: 30px auto;
   background-color: white;
-  margin: 40px 40px 10px 40px;
-`
+`;
 
-// Header
-const HeaderCon = styled.div`
-  text-align: center;
-`
-
-const HeaderLogo = styled.h1`
-  margin-bottom: 15px;
-`
-const HeaderP = styled.div`
-  margin-bottom: 15px;
-  font-weight: bold;
-  color: #8e8e8e;
-  font-size: 18px;
-`
-const HeaderBtn = styled.button`
-  margin-bottom: 15px;
-  width: inherit;
-  padding: 10px 20px;
-  border-radius: 5px;
-  border: none;
-  background-color: #0095f6;
-  color: #ffffff;
-`
-
-// Middle
-const MiddleCon = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  color: #8e8e8e;
-`
-
-// Bottom
-const BottomCon = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const BottomInput = styled.input`
-  width: inherit;
-  border: 1px solid #dbdbdb;
-  background-color: #fafafa;
-  padding: 10px 5px;
-  margin: 5px 0;
-`
-const BottonBtn = styled.button`
-  width: inherit;
-  margin-bottom: 20px;
-  padding: 7px 20px;
-  border-radius: 5px;
-  border: none;
-  background-color: #0095f6;
-  color: #ffffff;
-`
-const BottonLi = styled.li`
-  display: inline;
-  color: #8e9096;
-`
-const BottonA = styled.a`
-  text-decoration: none;
-  font-weight: bold;
-  color: #8e9096;
-`
-const BottomA1 = styled.a`
-  text-decoration: none;
-  color: #00a0f7;
-`
-const BottomOtherApp = styled.div`
-  text-align: center;
-`
-const BottomOtherAppP = styled.p`
-  margin-bottom: 15px;
-`
-const BottomOtherBtn = styled.button`
-   cursor: pointer;
-  border: none;
-  border-radius: 5px;
-  background-color: black;
-  color: white;
-  padding: 10px;
-  font-size: 18px;
-`
-
-// Footer
-const Footer = styled.div`
-  bottom: 0;
-  height: 2.5rem;
-  margin-top: 50px;
-`
-const FooterUi = styled.ul`
-  text-align: center;
-`
-const FooterLi = styled.li`
-  display: inline;
-  margin-right: 5px;
-`
-const FooterA = styled.a`
-  text-decoration: none;
-  font-size: 12px; 
-  color: #00376b;
-`
-const FooterP = styled.p`
-  margin: 10px 0;
-  text-align: center;
-  color: #8e9096;
-  font-size: 14px;
-`
